ae2fb079af1266d9aa8c83b739e5cfacc1605b7e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-08-21 16:28:30 +0000

    Deploy abel1105/booking to github.com/abel1105/booking.git:gh-pages

---
+++ precache-manifest.97f5b2a28c7650f1d8f9a5579e36b6e3.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3533c9f504f2132b49bb18c5a30e66e",
+    "url": "/booking/index.html"
+  },
+  {
+    "revision": "3fdc7544bc986ec5a9c8",
+    "url": "/booking/static/css/2.6cef9e96.chunk.css"
+  },
+  {
+    "revision": "a12866154fad3569de43",
+    "url": "/booking/static/css/main.0cc95db8.chunk.css"
+  },
+  {
+    "revision": "3fdc7544bc986ec5a9c8",
+    "url": "/booking/static/js/2.1c6ffd46.chunk.js"
+  },
+  {
+    "revision": "a12866154fad3569de43",
+    "url": "/booking/static/js/main.e0e58c23.chunk.js"
+  },
+  {
+    "revision": "e02b86460810afbfe6ea",
+    "url": "/booking/static/js/runtime~main.8af3e297.js"
+  },
+  {
+    "revision": "929508038ccc98ec0099bb614307a762",
+    "url": "/booking/static/media/bar.92950803.svg"
+  },
+  {
+    "revision": "c4c2c9720464acf58e319946a14524bf",
+    "url": "/booking/static/media/breakfast.c4c2c972.svg"
+  },
+  {
+    "revision": "410d1c428a8a48ed5019e12476225018",
+    "url": "/booking/static/media/breeze.410d1c42.svg"
+  },
+  {
+    "revision": "c76c9ad5e78ae01d0d3fa0dafc4f71fc",
+    "url": "/booking/static/media/crawling-baby-silhouette.c76c9ad5.svg"
+  },
+  {
+    "revision": "6a06afa106f27c964bedd26ed0b5f0a0",
+    "url": "/booking/static/media/dog.6a06afa1.svg"
+  },
+  {
+    "revision": "a1b1533cc5299b0bfc0fbf8ead74352a",
+    "url": "/booking/static/media/envelope-solid.a1b1533c.svg"
+  },
+  {
+    "revision": "7df7570f7e5bb9b2f7dc29aded8c7238",
+    "url": "/booking/static/media/facebook-square-brands.7df7570f.svg"
+  },
+  {
+    "revision": "a08b40e03859455b9dfbb8fe81fdf331",
+    "url": "/booking/static/media/fridge.a08b40e0.svg"
+  },
+  {
+    "revision": "1600889cd9dea93d957dd508ab0a4976",
+    "url": "/booking/static/media/home-solid.1600889c.svg"
+  },
+  {
+    "revision": "cc5e92b0767b99437ee32d191c9c2342",
+    "url": "/booking/static/media/instagram-brands.cc5e92b0.svg"
+  },
+  {
+    "revision": "d4ac21609a8dcd4a84f625091b1cf5c1",
+    "url": "/booking/static/media/logo_block.d4ac2160.svg"
+  },
+  {
+    "revision": "2540fc08d2e0e1afb6ccf1f049970d93",
+    "url": "/booking/static/media/logo_white.2540fc08.svg"
+  },
+  {
+    "revision": "ad2b689a8ee5572de3bcf622be325a7c",
+    "url": "/booking/static/media/mountain-range.ad2b689a.svg"
+  },
+  {
+    "revision": "74436c499fb4931b1afd28066d3e9b2d",
+    "url": "/booking/static/media/no-smoke-symbol.74436c49.svg"
+  },
+  {
+    "revision": "6dbf65e5c980bded8b6b6ca3b025af3f",
+    "url": "/booking/static/media/phone-alt-solid.6dbf65e5.svg"
+  },
+  {
+    "revision": "16543ec913c147480a05482652e096dc",
+    "url": "/booking/static/media/phone.16543ec9.svg"
+  },
+  {
+    "revision": "ef31df00d2233ac153e23339d52ce614",
+    "url": "/booking/static/media/room_service.ef31df00.svg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/booking/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/booking/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/booking/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/booking/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "6228e404b5061e78c0b1cf5da862cdbb",
+    "url": "/booking/static/media/sofa.6228e404.svg"
+  },
+  {
+    "revision": "5e18caa0144b7e760ada9d9c4be6f6d3",
+    "url": "/booking/static/media/tick-inside-circle.5e18caa0.svg"
+  },
+  {
+    "revision": "7e9c444b1cb139df81d88466b495153b",
+    "url": "/booking/static/media/wifi.7e9c444b.svg"
+  }
+]);
